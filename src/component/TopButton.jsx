d4ac51e9e37b0b1ff3eeb2586203569ac5d3fd87
rbngm <rabinghimire2012@gmail.com>
2023-04-05 10:39:30 +0800

    initial setup

---
+++ src/component/TopButton.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function TopButton() {
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 2,
+      title: "Sydney",
+    },
+    {
+      id: 3,
+      title: "Melbourne",
+    },
+    {
+      id: 4,
+      title: "Perth",
+    },
+    {
+      id: 5,
+      title: "Brisbane",
+    },
+  ];
+
+  return;
+  <div className="flex items-center justify-around my-6">
+    {cities.map((city) => {
+      <button className="text-white text-lg font-medium">{city.title}</button>;
+    })}
+  </div>;
+}
+
+export default TopButton;
