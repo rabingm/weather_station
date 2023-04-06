6fecd5e087226932b289d2fecd21e3141a78876c
rbngm <rabinghimire2012@gmail.com>
2023-04-06 13:14:01 +0800

    search, location and temperature unit chnages functions added

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButton setQuery={setQuery}/>
-      <Inputs />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
       {weather && (
         <div>
--- src/component/Inputs.jsx
+++ src/component/Inputs.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
-const Inputs = ({ setQuery, units, setUnits }) => {
+const Inputs = ({ setQuery, units, setUnits })=>{
   const [city, setCity] = useState("");
 
   const handleSearchClick = () => {
-    if (city !== "") {
-      setQuery({ q: city });
-    }
+    if (city !== "") setQuery({ q: city });
   };
+
+  const handleLocationClick = () =>{
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position)=>{
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+
+        setQuery({
+          lat,
+          lon
+        })
+      })
+    }
+  }
+
+  const handleUnitsChange= (e) =>{
+    const selectedUnit = e.currentTarget.name
+    if (units !== selectedUnit) setUnits(selectedUnit)
+  }
 
   return (
     <div className="flex flex-row justify-center my-6">
@@ -28,12 +45,14 @@ const Inputs = ({ setQuery, units, setUnits }) => {
         <UilLocationPoint
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleLocationClick}
         />
       </div>
       <div className="flex flex-row w-1/4 items-center justify-center">
         <button
           name="metric"
           className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleUnitsChange}
         >
           °C
         </button>
@@ -41,6 +60,7 @@ const Inputs = ({ setQuery, units, setUnits }) => {
         <button
           name="imperial"
           className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleUnitsChange}
         >
           °F
         </button>
